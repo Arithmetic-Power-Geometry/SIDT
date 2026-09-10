d2b9500c167112f3cb444a61c067d5e75d77b646
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-10 18:31:10 +0530

    Rebuild SIDT package exports

--- src/sidt/__init__.py
+++ src/sidt/__init__.py
@@ -1,2 +1,3 @@
-from .core import gf2_rank, affine_sid, locality_lower_bound, collapse_budget_lower_bound
-__all__ = ["gf2_rank","affine_sid","locality_lower_bound","collapse_budget_lower_bound"]
+from .core import gf2_rank, affine_sid, locality_lower_bound, domain_collapse_lower_bound, approx_treewidth, exact_solution_count, sid_from_count
+from .parsers import parse_dimacs, parse_fukuoka_gf2
+__all__ = ["gf2_rank","affine_sid","locality_lower_bound","domain_collapse_lower_bound","approx_treewidth","exact_solution_count","sid_from_count","parse_dimacs","parse_fukuoka_gf2"]
